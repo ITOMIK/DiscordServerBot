342f9921563b48ebd66404a89968028e97d48edc
ITOMIK <49075130+ITOMIK@users.noreply.github.com>
2023-03-19 21:24:29 +0200

    new token

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-token= 'MTA4MzczNzgwODYzNzc5NjQyMw.GpqysK.n84zB1andiXpBCFAUIbWwgBj98F4eRbOQLkZXQ'
+token= 'MTA4MzczNzgwODYzNzc5NjQyMw.GpyRvD.rmMbEDcT9_rxYYfuExxUxCNQFfCG20jscPiaTc'
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix = '!', intents=intents)
@@ -10,5 +10,6 @@ bot = commands.Bot(command_prefix = '!', intents=intents)
 async def hello(ctx, member : discord.Member): # Создаём функцию и передаём аргумент ctx.
     author = ctx.message.author # Объявляем переменную author и записываем туда информацию об авторе.
     await ctx.send(f"name{member.nick}")
+    print(member.guild._voice_states)
 
 bot.run(token) 
