f6f8a75fee4f8d4406f84f4d4cace8490a4523bf
IlyaCoolDev <ilya@d3en.ru>
2023-03-19 22:38:53 +0300

    move muted

--- main.py
+++ main.py
@@ -11,12 +11,10 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def hello(ctx, member: discord.Member):  # Создаём функцию и передаём аргумент ctx.
     author = ctx.message.author  # Объявляем переменную author и записываем туда информацию об авторе.
     await ctx.send(f"name{member.nick}")
-    muted = []
-    for mem in member.guild._voice_states:
-        state = member.guild._voice_states[mem]
-        if state.self_mute:
-            muted.append(mem)
-    await ctx.send(muted)
+    if member.id in member.guild._voice_states.keys():
+        is_mute = member.guild._voice_states[member.id].self_mute
+        if is_mute:
+            await member.edit(voice_channel=member.guild.voice_channels[1])
 
 
 bot.run(token)
