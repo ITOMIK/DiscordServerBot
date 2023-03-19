0226794955810bf8321de5f2c8e27b915dc948da
IlyaCoolDev <ilya@d3en.ru>
2023-03-19 22:32:35 +0300

    return muted people

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import discord
 from discord.ext import commands
 
-token= 'MTA4MzczNzgwODYzNzc5NjQyMw.GpyRvD.rmMbEDcT9_rxYYfuExxUxCNQFfCG20jscPiaTc'
+token = 'MTA4MzczNzgwODYzNzc5NjQyMw.GpyRvD.rmMbEDcT9_rxYYfuExxUxCNQFfCG20jscPiaTc'
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix = '!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.command()  
-async def hello(ctx, member : discord.Member): # Создаём функцию и передаём аргумент ctx.
-    author = ctx.message.author # Объявляем переменную author и записываем туда информацию об авторе.
+
+@bot.command()
+async def hello(ctx, member: discord.Member):  # Создаём функцию и передаём аргумент ctx.
+    author = ctx.message.author  # Объявляем переменную author и записываем туда информацию об авторе.
     await ctx.send(f"name{member.nick}")
-    print(member.guild._voice_states)
+    muted = []
+    for mem in member.guild._voice_states:
+        state = member.guild._voice_states[mem]
+        if state.self_mute:
+            muted.append(mem)
+    await ctx.send(muted)
 
-bot.run(token) 
+
+bot.run(token)
