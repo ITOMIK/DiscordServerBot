98686ebc225193a474128399b8cbf55bfc686d80
IlyaCoolDev <ilya@d3en.ru>
2023-03-19 23:50:52 +0300

    change bot_status

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import time
 
 import discord
-from discord import Member, VoiceChannel
+from discord import Member, VoiceChannel, Status
 from discord.ext import commands
 
 token = 'MTA4MzczNzgwODYzNzc5NjQyMw.GpyRvD.rmMbEDcT9_rxYYfuExxUxCNQFfCG20jscPiaTc'
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents, activity=discord.Game(name="!hello; !hello_all"))
 
 
 def get_deaf(member):
