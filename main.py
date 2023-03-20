0452303347a3a100fe5d383e4f44a517fa7d4f0e
IlyaCoolDev <ilya@d3en.ru>
2023-03-20 22:52:46 +0300

    create async moving

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 
 import discord
@@ -9,75 +10,76 @@ token = 'MTA4MzczNzgwODYzNzc5NjQyMw.GpyRvD.rmMbEDcT9_rxYYfuExxUxCNQFfCG20jscPiaT
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents, activity=discord.Game(name="!helpme"))
-listOfMem = []
+deaf_members = []
 
-def get_deaf(member):
+
+def get_deaf(member: Member) -> bool:
     return member.voice.self_deaf
 
-def listeningMoveList(list):
-    if(len(list)>0):
-        for i in list:
-            if(i.voice.self_deaf):
-                move(i)
-            else:
-                list.remove(i)
 
-async def move(member, check_function):
-    channel = member.voice.channel.id
-    while check_function(member):
-        await member.edit(voice_channel=member.guild.voice_channels[-1])
-        time.sleep(1)
-        if not check_function(member):
-            break
-        await member.edit(voice_channel=member.guild.voice_channels[-2])
-        time.sleep(1)
-    await member.edit(voice_channel=member.guild.get_channel(channel))
+@bot.event
+async def on_ready():
+    bot.loop.create_task(move_deaf(get_deaf))
+    print("started")
 
 
-async def move_all(members: list[Member], check_function):
+async def move_all(members: list[Member]):
+    global deaf_members
     mem_ch: list[tuple[Member, int]] = [(mem, mem.voice.channel.id) for mem in members]
-    while len(mem_ch) > 0:
-        for member in mem_ch:
-            if not check_function(member[0]):
-                await member[0].edit(voice_channel=member[0].guild.get_channel(member[1]))
-                mem_ch.remove(member)
-                continue
-            await member[0].edit(voice_channel=member[0].guild.voice_channels[-1])
-        time.sleep(1)
-        for member in mem_ch:
-            if not check_function(member[0]):
-                await member[0].edit(voice_channel=member[0].guild.get_channel(member[1]))
-                mem_ch.remove(member)
-                continue
-            await member[0].edit(voice_channel=member[0].guild.voice_channels[-2])
-        time.sleep(1)
+    for i in mem_ch:
+        deaf_members.append(i)
 
-
-thread1 = Thread(target=listeningMoveList, args=(listOfMem))
 
 @bot.command()
-async def hello(ctx, member : discord.Member = None):  # Создаём функцию и передаём аргумент ctx.
+async def hello(ctx, member: discord.Member = None):  # Создаём функцию и передаём аргумент ctx.
+    global deaf_members
     author = ctx.message.author  # Объявляем переменную author и записываем туда информацию об авторе.
     try:
-        if(member==None):
+        if member is None:
             await ctx.send(f"вы не ввели имя")
         else:
             if member.id in member.guild._voice_states.keys():
                 if get_deaf(member):
-                    listOfMem.append(member)
+                    deaf_members.append((member, member.voice.channel.id))
     except discord.ext.commands.errors.MemberNotFound(member):
-            print("Пользователь не найден!")
+        print("Пользователь не найден!")
+
 
 @bot.command()
-async def hello_all(ctx, ):
+async def hello_all(ctx):
+    global deaf_members
+    print("hello_all")
     author: Member = ctx.message.author
     members = author.guild.voice_states.keys()
-    await move_all([author.guild.get_member(member_key) for member_key in members], get_deaf)
+    mem_ch: list[tuple[Member, int]] = [(author.guild.get_member(mem), author.guild.get_member(mem).voice.channel.id)
+                                        for mem in members]
+    for i in mem_ch:
+        deaf_members.append(i)
+
 
 @bot.command()
 async def helpme(ctx):
-     await ctx.send(f"!hello @UserName - кидает из канала в канал ЗАМУЧЕНОГО человека до тех пор пока он не размутиться, !hello_all - кидает из канала в канал всех замученых, находящихся с вами в одном канале до тех пор, пока они не размутятся")    
+    await ctx.send(
+        f"!hello @UserName - кидает из канала в канал ЗАМУЧЕНОГО человека до тех пор пока он не размутиться, !hello_all - кидает из канала в канал всех замученых, находящихся с вами в одном канале до тех пор, пока они не размутятся")
 
+
+async def move_deaf(check_function):
+    global deaf_members
+    while True:
+        for member in deaf_members:
+            if not check_function(member[0]):
+                await member[0].edit(voice_channel=member[0].guild.get_channel(member[1]))
+                deaf_members.remove(member)
+                continue
+            await member[0].edit(voice_channel=member[0].guild.voice_channels[-1])
+        await asyncio.sleep(1)
+        for member in deaf_members:
+            if not check_function(member[0]):
+                await member[0].edit(voice_channel=member[0].guild.get_channel(member[1]))
+                deaf_members.remove(member)
+                continue
+            await member[0].edit(voice_channel=member[0].guild.voice_channels[-2])
+        await asyncio.sleep(1)
 
 
 bot.run(token)
